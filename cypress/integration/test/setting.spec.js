f8183083deb0f0bb17908ba3b476f36f053abe9e
idjaeha <jaehanim1@gmail.com>
2022-04-12 18:41:25 +0900

    다양한 테스트 추가

--- cypress/integration/test/setting.spec.js
+++ cypress/integration/test/setting.spec.js
@@ -5,7 +5,17 @@ Cypress.Commands.add("setCorrectCarName", () => {
   cy.get(".__setting_count_fieldset").should("not.have.class", "d-none");
 });
 
-describe("example to-do app", () => {
+Cypress.Commands.add("setCorrectCarCount", () => {
+  cy.get(".__setting_count_input").type("10{enter}");
+  cy.get(".__process_section").should("not.have.class", "d-none");
+});
+
+const ALERT_MSG_DUPLICATE_NAME = "자동차 이름은 중복될 수 없습니다.";
+const ALERT_MSG_INVALID_NAME = "자동차 이름은 1자 이상, 5자 이하여야합니다.";
+const ALERT_MSG_NAN_COUNT = "시도할 횟수는 숫자만 입력할 수 있습니다.";
+const ALERT_MSG_INVALID_COUNT = "시도할 횟수는 0이상이여야합니다.";
+
+describe("게임 설정 테스트", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
@@ -17,21 +27,28 @@ describe("example to-do app", () => {
   it("공백을 입력했을 때 경고창이 나타난다.", () => {
     cy.get(".__setting_name_input").type("{enter}");
     cy.on("window:alert", (text) => {
-      expect(text).to.contains("자동차 이름은 1자 이상, 5자 이하여야합니다.");
+      expect(text).to.contains(ALERT_MSG_INVALID_NAME);
     });
   });
 
   it("올바르지 못한 입력을 입력하면 경고창이 나타난다.", () => {
     cy.get(".__setting_name_input").type(",,,{enter}");
     cy.on("window:alert", (text) => {
-      expect(text).to.contains("자동차 이름은 1자 이상, 5자 이하여야합니다.");
+      expect(text).to.contains(ALERT_MSG_INVALID_NAME);
+    });
+  });
+
+  it("자동차 이름의 조건에 맞지 않으면 경고창이 나타난다.", () => {
+    cy.get(".__setting_name_input").type("555555,6666,777,9{enter}");
+    cy.on("window:alert", (text) => {
+      expect(text).to.contains(ALERT_MSG_INVALID_NAME);
     });
   });
 
   it("중복된 이름을 입력했을 때 경고창이 나타난다.", () => {
     cy.get(".__setting_name_input").type("1, 1{enter}");
     cy.on("window:alert", (text) => {
-      expect(text).to.contains("자동차 이름은 중복될 수 없습니다.");
+      expect(text).to.contains(ALERT_MSG_DUPLICATE_NAME);
     });
   });
 
@@ -43,7 +60,7 @@ describe("example to-do app", () => {
     cy.setCorrectCarName();
     cy.get(".__setting_count_input").type("{enter}");
     cy.on("window:alert", (text) => {
-      expect(text).to.contains("시도할 횟수는 숫자만 입력할 수 있습니다.");
+      expect(text).to.contains(ALERT_MSG_NAN_COUNT);
     });
   });
 
@@ -51,7 +68,7 @@ describe("example to-do app", () => {
     cy.setCorrectCarName();
     cy.get(".__setting_count_input").type("-1{enter}");
     cy.on("window:alert", (text) => {
-      expect(text).to.contains("시도할 횟수는 0이상이여야합니다.");
+      expect(text).to.contains(ALERT_MSG_INVALID_COUNT);
     });
   });
 
@@ -59,13 +76,12 @@ describe("example to-do app", () => {
     cy.setCorrectCarName();
     cy.get(".__setting_count_input").type("-{enter}");
     cy.on("window:alert", (text) => {
-      expect(text).to.contains("시도할 횟수는 숫자만 입력할 수 있습니다.");
+      expect(text).to.contains(ALERT_MSG_NAN_COUNT);
     });
   });
 
   it("올바른 숫자를 입력하면 다음 창으로 이동한다.", () => {
     cy.setCorrectCarName();
-    cy.get(".__setting_count_input").type("10{enter}");
-    cy.get(".__process_section").should("not.have.class", "d-none");
+    cy.setCorrectCarCount();
   });
 });
